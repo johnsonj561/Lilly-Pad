e7e7ede0c18746c44c542aa11a446de1b3c8a273
Justin Johnson <johnsonj561@gmail.com>
2015-10-10 09:38:32 -0400

    1st Commit

---
+++ index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>The Lilly Pad</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript">var switchTo5x=true;</script>
+    <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+    <script type="text/javascript" src="http://s.sharethis.com/loader.js"></script>
+  </head>
+  <body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-static-top" role="navigation">  <!--removed navbar-fixed-top -->
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <!--     <a class="navbar-brand" href="index.html">The Lilly Pad</a>   -->
+          <img class="img-responsive top-logo" src="img/lilly-pad-logo-v2-300x100.png"/><a name="top"></a>
+          <img class="img-responsive top-logo-mobile" src="img/lilly-pad-logo-240x70.png"/>
+          <!--      <a class="btn btn-lg btn-default btn-block help-button" href="#">1-800-256-1888</a>    -->
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <a href="index.php">Home</a>
+            </li>
+            <li>
+              <a href="#home-page-1">About</a>
+            </li>
+            <li class="mobile-only">
+              <a href="#">Properties</a>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Properties<b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a href="#">Main Campus</a>
+                </li>
+                <li>
+                  <a href="#">Cordova Executive</a>
+                </li>
+                <li>
+                  <a href="#">Flagler Executive</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">Application</a>
+            </li>
+            <li>
+              <a href="https://lillypad.gocashbox.com/" target="_blank">Pay Online</a>
+            </li>
+            <li>
+              <a href="view/contact.html">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+      <!-- slider -->
+      <div class="col-xs-12">
+        <div class="fadein">
+          <img class = "img-responsive" src="img/slider-beach-spiritual-freedom-1100x500.jpg">
+          <img class = "img-responsive" src="img/lily-pad-flower-2-1100x500.jpg">
+          <img class = "img-responsive" src="img/spiritual-sunlight-freedom-1100x500.jpg">
+          <img class = "img-responsive" src="img/west-palm-beach-sunset-1100x500.jpg">
+          <img class = "img-responsive" src="img/lily-pad-flower-1100x500.jpg">
+        </div>
+      </div>
+      <!-- down-scroll-1 -->
+      <div class="col-lg-12 text-center down-scroll-1">
+        <p class="down-glyph"><a href="#home-page-1">&#65086;</a></p>
+      </div>
+
+
+
+      <div class="row home-page-1" name="about" id="about"><a name="home-page-1"></a>
+        <div class="col-md-4 col-sm-6 hero-feature">
+          <div class="thumbnail">
+            <img src="img/west-palm-beach-spirituality-500x250.jpg" alt="">
+            <div class="caption">
+              <h1 class="thumbnail-title text-center">West Palm Beach, Florida</h1>
+              <p class="image-description">We offer a variety of living arrangements for women in recovery. Structure and safety are essential to
+                laying the foundation to long term sobriety. Our campuses are conveniently located near down town West Palm 
+                Beach, with easy access to numerous AA/NA club houses, job opportunities, and our beautiful beaches</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6 hero-feature">
+          <div class="thumbnail">
+            <img src="img/flagler-executive-3-500x250.jpg" alt="">
+            <div class="caption">
+              <h1 class="thumbnail-title text-center">Affordable Housing Options</h1>
+              <p class="image-description">We have multiple living arrangements available, allowing you to choose the option that best suits your current circumstances.
+                Our Main campus consists of 3 properties located in central West Palm Beach. In addition, our executive housing gives the option
+                of an up-scale living environment at an increased rate. All housing maintains 24/7 staff support to assist in the recovery
+                process.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6 hero-feature">
+          <div class="thumbnail">
+            <img src="img/girls-in-sobriety-500x250.jpg" alt="">
+            <div class="caption">
+              <h1 class="thumbnail-title text-center">All Women's Fellowship</h1>
+              <p class="image-description">Experience shows that an all women's living environment has the best results when laying the foundation in early sobriety.
+                We participate in regular activities to bring the community together and encourage women's fellowship to tighten our bonds
+                and support each other along the journey to freedom from drugs and alcohol.<p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-12 row">
+        <div class="col-lg-2">
+          <p class="to-top pull-left"><a href="#top">Back To Top</a></p>
+        </div>
+        <div class="col-lg-8"></div>
+        <div class="col-lg-2">
+          <img class="img-responsive pull-left farr-logo" src="img/farr-logo-120x71.png"/>
+        </div>
+      </div> 
+      <div class="row social-media-div">
+        <row class="text-center social-media">
+          <ul class="share-buttons">
+            <li><a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.kbsoftwaresolutions.com&t=" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.URL) + '&t=' + encodeURIComponent(document.URL)); return false;"><img src="img/social-media/Facebook.png" alt="facebook-logo"></a></li>
+            <li><a href="https://twitter.com/intent/tweet?source=http%3A%2F%2Fwww.kbsoftwaresolutions.com&text=:%20http%3A%2F%2Fwww.kbsoftwaresolutions.com" target="_blank" title="Tweet" onclick="window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(document.title) + ':%20'  + encodeURIComponent(document.URL)); return false;"><img src="img/social-media/Twitter.png" alt="twitter-logo"></a></li>
+            <li><a href="https://plus.google.com/share?url=http%3A%2F%2Fwww.kbsoftwaresolutions.com" target="_blank" title="Share on Google+" onclick="window.open('https://plus.google.com/share?url=' + encodeURIComponent(document.URL)); return false;"><img src="img/social-media/Google+.png" alt="google-logo"></a></li>
+            <li><a href="http://pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.kbsoftwaresolutions.com&description=" target="_blank" title="Pin it" onclick="window.open('http://pinterest.com/pin/create/button/?url=' + encodeURIComponent(document.URL) + '&description=' +  encodeURIComponent(document.title)); return false;"><img src="img/social-media/Pinterest.png" alt="pinterest-logo"></a></li>
+            <li><a href="mailto:?subject=&body=:%20http%3A%2F%2Fwww.kbsoftwaresolutions.com" target="_blank" title="Email" onclick="window.open('mailto:?subject=' + encodeURIComponent(document.title) + '&body=' +  encodeURIComponent(document.URL)); return false;"><img src="img/social-media/Email.png" alt="email-logo"></a></li>
+          </ul>
+        </row>
+      </div>
+      <footer>
+        <div class="container">
+          <div class="row">
+            <div class=" row col-lg-12">
+              <div class="col-lg-7">
+                <ul class="list-inline">
+                  <li>
+                    <a href="index.php">Home</a>
+                  </li>
+                  <li class="footer-menu-divider">&sdot;</li>
+                  <li>
+                    <a href="#home-page-1">About</a>
+                  </li>
+                  <li class="footer-menu-divider">&sdot;</li>
+                  <li>
+                    <a href="view/properties.php">Properties</a>
+                  </li>
+                  <li class="footer-menu-divider">&sdot;</li>
+                  <li>
+                    <a href="view/application.php">Application</a>
+                  </li>
+                  <li class="footer-menu-divider">&sdot;</li>
+                  <li>
+                    <a href="https://lillypad.gocashbox.com/">Pay Online</a>
+                  </li>
+                  <li class="footer-menu-divider">&sdot;</li>
+                  <li>
+                    <a href="view/contact.php">Contact</a>
+                  </li>
+                  <li class="footer-menu-divider">&sdot;</li>
+
+                </ul>
+                <p class="copyright text-muted medium">Copyright &copy; The Lilly Pad West Palm Beach, All Rights Reserved</p>
+              </div>
+              <div class="col-lg-5">
+                <p class="pull-right kb-footer-text">
+                  <img src="img/kb-logo-v3-75x60.png" alt="KB Software Solutions Web Design">
+                  <a href="http://www.kbsoftwaresolutions.com" target="_blank">Website Development by KB Software Solutions</a>
+                </p><a name="home-page-3"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script>
+      $(function(){
+        $('.fadein img:gt(0)').hide();
+        setInterval(function(){
+          $('.fadein :first-child').fadeOut()
+          .next('img').fadeIn()
+          .end().appendTo('.fadein');}, 
+                    5000);
+      });
+      $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      }); 
+      $(document).ready(function(){
+        $('.dropdown').hover(function(){
+          $(this).find('ul').stop().slideToggle(300);
+        });
+      });
+    </script>
+    <script type="text/javascript">stLight.options({publisher: "f62e515e-8f42-4815-aec5-77d2c83198b5", doNotHash: false, doNotCopy: false, hashAddressBar: false});
+    </script>
+    <script>
+      var options={ "publisher": "f62e515e-8f42-4815-aec5-77d2c83198b5", "position": "left", "ad": { "visible": false, "openDelay": 5, "closeDelay": 0}, "chicklets": { "items": ["facebook", "twitter", "linkedin", "email", "googleplus"]}};
+      var st_hover_widget = new sharethis.widgets.hoverbuttons(options);
+    </script>
+  </body>
+</html>
