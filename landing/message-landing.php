77a620228ddfe069447d24164e6b519bce78f3df
Justin Johnson <johnsonj561@gmail.com>
2015-11-11 20:45:22 -0500

    Navbar updated

--- landing/message-landing.php
+++ landing/message-landing.php
@@ -3,14 +3,14 @@ $name = strip_tags($_POST['name']);
 $email = strip_tags($_POST['email']);
 $phoneNumber = strip_tags($_POST['phoneNumber']);
 $message = strip_tags($_POST['message']);
-$headers = "From: www.kbsoftwaresolutions.com\r\n";
+$headers = "From: www.wpblillypad.com\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
 $emailMessage = 
   "<table class='form-email-message'>
       <tr>
-        <td>KB Software Solutions</td>
+        <td>Lilly Pad Messaging Center</td>
       </tr>
       <tr>
         <td>$message</td>
@@ -26,7 +26,7 @@ $emailMessage =
       </tr>
     </table>";
 
-$result = mail("jjohnson@ambrosiatc.com", "KB Software Solutions", $emailMessage, $headers);
+$result = mail("sivia@thelillypad.org", "Lilly Pad Messaging Center", $emailMessage, $headers);
 ?>
 
 <!DOCTYPE html>
@@ -48,63 +48,103 @@ $result = mail("jjohnson@ambrosiatc.com", "KB Software Solutions", $emailMessage
   <body>
     <?php require_once('../template/navbar.html'); ?>
     <a  name="services"></a>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-5 col-sm-6">
-            <hr class="section-heading-spacer">
-            <div class="clearfix"></div>
-            <h1 class="section-heading text-center">Message Received</h1>
-            <hr class="paragraph-divider">
-            <h3 class="text-center">Thank you for your interest in KB Software Solutions</h3>
-            <h3 class="text-center"><a href="../index.php">Return to Home Page</a></h3>
-          </div>
-          <div class="col-lg-5 col-lg-offset-2 col-sm-6">
-            <img class="img-responsive" src="../img/mail-received.png" alt="">
-          </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 col-sm-6">
+          <hr class="section-heading-spacer">
+          <div class="clearfix"></div>
+          <h1 class="section-heading text-center">Message Received</h1>
+          <hr class="paragraph-divider">
+          <h3 class="text-center">A staff member will get back to you shortly.</h3>
+          <h3 class="text-center"><a href="../index.php">Return to Home Page</a></h3>
         </div>
-        <hr class="paragraph-divider">
-      </div>
-
-    <!-- Footer -->
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <?php require_once('../template/social-media.html'); ?>
-            <div class="col-lg-6">
-              <ul class="list-inline">
-                <li>
-                  <a href="../index.php">Home</a>
-                </li>
-                <li class="footer-menu-divider">&sdot;</li>
-                <li>
-                  <a href="about.php">About</a>
-                </li>
-                <li class="footer-menu-divider">&sdot;</li>
-                <li>
-                  <a href="services.php">Services</a>
-                </li>
-                <li class="footer-menu-divider">&sdot;</li>
-                <li>
-                  <a href="contact.php">Contact</a>
-                </li>
-              </ul>
-              <p class="copyright text-muted small">Copyright &copy; KB Software Solutions 2015. All Rights Reserved</p>
-            </div>
-            <div class="col-lg-6">
-              <p class="copyright text-muted medium pull-right">
-                <img src="../img/kb-logo-v3-75x60.png" alt="KB Software Solutions Logo">
-                <a href="http://www.kbsoftwaresolutions.com">Website Development by KB Software Solutions</a>
-              </p>
-            </div>
-          </div>
+        <div class="col-lg-5 col-lg-offset-2 col-sm-6">
+          <img class="img-responsive" src="../img/mail-received.png" alt="">
         </div>
       </div>
-    </footer>
-    <!-- jQuery -->
-    <script src="../js/jquery.js"></script>
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/slide-toggle-nav.js"></script>
+      <hr class="paragraph-divider">
+    </div>
   </body>
+
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class=" row col-lg-12">
+          <div class="col-lg-7">
+            <ul class="list-inline">
+              <li>
+                <a href="../index.php">Home</a>
+              </li>
+              <li class="footer-menu-divider">&sdot;</li>
+              <li>
+                <a href="../index.php#home-page-1">About</a>
+              </li>
+              <li class="footer-menu-divider">&sdot;</li>
+              <li>
+                <a href="properties.php">Properties</a>
+              </li>
+              <li class="footer-menu-divider">&sdot;</li>
+              <li>
+                <a href="application.php">Application</a>
+              </li>
+              <li class="footer-menu-divider">&sdot;</li>
+              <li>
+                <a target="_blank" href="https://lillypad.gocashbox.com/">Pay Online</a>
+              </li>
+              <li class="footer-menu-divider">&sdot;</li>
+              <li>
+                <a href="contact.php">Contact</a>
+              </li>
+              <li class="footer-menu-divider">&sdot;</li>
+
+            </ul>
+            <p class="copyright text-muted medium">Copyright &copy; The Lilly Pad West Palm Beach, All Rights Reserved</p>
+          </div>
+          <div class="col-lg-5">
+            <p class="pull-right kb-footer-text">
+              <img src="../img/kb-logo-v3-75x60.png" alt="KB Software Solutions Web Design">
+              <a href="http://www.kbsoftwaresolutions.com" target="_blank">Website Development by KB Software Solutions</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <script src="../js/jquery.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script>
+    $(function(){
+      $('.fadein img:gt(0)').hide();
+      setInterval(function(){
+        $('.fadein :first-child').fadeOut()
+        .next('img').fadeIn()
+        .end().appendTo('.fadein');}, 
+                  5000);
+    });
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    }); 
+    $(document).ready(function(){
+      $('.dropdown').hover(function(){
+        $(this).find('ul').stop().slideToggle(300);
+      });
+    });
+  </script>
+  <script type="text/javascript">stLight.options({publisher: "f62e515e-8f42-4815-aec5-77d2c83198b5", doNotHash: false, doNotCopy: false, hashAddressBar: false});
+  </script>
+  <script>
+    var options={ "publisher": "f62e515e-8f42-4815-aec5-77d2c83198b5", "position": "left", "ad": { "visible": false, "openDelay": 5, "closeDelay": 0}, "chicklets": { "items": ["facebook", "twitter", "linkedin", "email", "googleplus"]}};
+    var st_hover_widget = new sharethis.widgets.hoverbuttons(options);
+  </script>
 </html>
