3258200fc729eb18b63f08dc2f8627ec5ba960ee
Justin Johnson <johnsonj561@gmail.com>
2015-11-14 13:55:18 -0500

    1st Commit - Page to include Main campus Executive and Step Down housing descriptions

---
+++ view/properties.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Housing Application</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/modern-business.css" rel="stylesheet">
+    <link href="../font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript">var switchTo5x=true;</script>
+    <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+    <script type="text/javascript" src="http://s.sharethis.com/loader.js"></script>
+  </head>
+  <body>
+    <a name="top"></a>
+    <?php require_once('../templates/navbar.html'); ?>
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="">
+        <img class="img-responsive" src="../img/spiritual-tools-message-v3.png" alt=""/>
+      </div>
+
+      <!-- Project One -->
+      <div class="row"><a name="main-campus"></a>
+        <div class="col-md-7">
+          <a href="portfolio-item.html">
+            <img class="img-responsive img-hover about-img" src="../img/main-campus-cover-700x400.jpg" alt="Main Campus Sober Living Halfway House West
+                                                                                                            Palm Beach Florida">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h2>Main Campus</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+          <a class="btn btn-primary" href="main-campus.php">View Images</i></a>
+      </div>
+    </div>
+    <!-- /.row -->
+
+    <hr>
+
+    <!-- Project Two -->
+    <div class="row"><a name="executive"></a>
+      <div class="col-md-7">
+        <a href="portfolio-item.html">
+          <img class="img-responsive img-hover about-img" src="../img/executive-cover-700x400.jpg" alt="Premium Executive Sober House for Women
+                                                                                                        In Recovery">
+        </a>
+      </div>
+      <div class="col-md-5">
+        <h2>Executive Housing</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, odit velit cumque vero doloremque repellendus distinctio maiores rem expedita a nam vitae modi quidem similique ducimus! Velit, esse totam tempore.</p>
+        <a class="btn btn-primary" href="executive.php">View Images</i></a>
+    </div>
+    </div>
+  <!-- /.row -->
+  <hr>
+  <!-- Project Three -->
+  <div class="row"><a name="step-down"></a>
+    <div class="col-md-7">
+      <a href="portfolio-item.html">
+        <img class="img-responsive img-hover about-img" src="../img/stepdown-cover-700x400.jpg" alt="Step Down - 3 Quarter Living for Women in Sobriety">
+      </a>
+    </div>
+    <div class="col-md-5">
+      <h2>Step Down Housing</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, temporibus, dolores, at, praesentium ut unde repudiandae voluptatum sit ab debitis suscipit fugiat natus velit excepturi amet commodi deleniti alias possimus!</p>
+      <a class="btn btn-primary" href="step-down.php">View Images</i></a>
+  </div>
+  </div>
+<!-- /.row -->
+<div class="col-lg-12 row">
+  <div class="col-lg-2">
+    <p class="to-top pull-left"><a href="#top">Back To Top</a></p>
+  </div>
+  <div class="col-lg-8"></div>
+  <div class="col-lg-2">
+    <img class="img-responsive pull-right farr-logo" src="../img/farr-logo-120x71.png"/>
+  </div>
+</div> 
+</div>
+<?php 
+require_once('../templates/social-footer.html');
+require_once('../templates/footer.html'); ?>
+</body>
+</html>
